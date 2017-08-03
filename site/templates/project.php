7d1acddebc35490a3020b5cc23f7c6d60c94952e
flomerboy <rmather@alumni.risd.edu>
2017-08-03 01:14:07 -0400

    lots of amazing changes and fully responsive

--- site/templates/project.php
+++ site/templates/project.php
@@ -18,7 +18,22 @@
       // above the files list in the sidebar.
       foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
         <figure>
-          <img src="<?= $image->url() ?>" alt="<?= $page->title()->html() ?>" />
+          <?php if($image->caption()->isNotEmpty()): ?>
+            <a href="<?=$image->link() ?>">
+          <?php endif ?>
+          <img src="<?= $image->url() ?>" alt="<?= $page->title()->html() ?>"
+          <?php if($image->maxwidth()->bool()): ?>
+            class="small-image"
+          <?php endif ?>
+          />
+          <?php if($image->caption()->isNotEmpty()): ?>
+          </a>
+          <?php endif ?>
+          <?php if($image->caption()->isNotEmpty()): ?>
+            <figcaption>
+              <?= $image->caption()->kirbytext() ?>
+            </figcaption>
+          <?php endif ?>
         </figure>
       <?php endforeach ?>
 
