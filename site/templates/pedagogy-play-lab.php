da86361c4161b26beda6914ee92f2e2b75fbce48
flomerboy <rmather@alumni.risd.edu>
2017-08-14 13:06:27 -0400

    ppl you have a home now

---
+++ site/templates/pedagogy-play-lab.php
@@ -0,0 +1,22 @@
+<?php snippet('header') ?>
+
+
+      <!--[if lt IE 8]>
+          <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+      <![endif]-->
+
+      <!-- Add your site or application content here -->
+      <div class="wrap wide">
+      <hr />
+      <h2>Pedagogy Play Lab</h2>
+      <p><br>A reading club about play, pedagogy, and learning meeting biweekly in Williamsburg, Brooklyn.</p>
+      <p>Sign up for the mailing list if you would like to be notified when meetings begin. Pedagogy Play Lab is a part of <a href="http://learning-gardens.co">Learning Gardens</a>. Join the #pedagogy-play-lab channel on Slack to participate in the online discussion.</p>
+      <a href="pedagogy-play-lab/schedule"><p class = "see-blog">Schedule</p></a> | 
+      <a href="http://eepurl.com/ciqYyT"><p class = "see-blog">Get our e-mails</p></a> |
+      <a href="https://lg-slack-automate.herokuapp.com/"><p class = "see-blog">Join our Slack</p></a>
+      <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+      <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+      <script src="js/plugins.js"></script>
+      <script src="js/main.js"></script>
+    </div>
+<?php snippet('footer') ?>
---
+++ site/templates/schedule.php
@@ -0,0 +1,83 @@
+<?php snippet('header') ?>
+<div class="wrap wide">
+
+<hr />
+<h2>Schedule</h2>
+<p><br>All events held near the Graham Ave. L train. <br><a href="http://eepurl.com/ciqYyT" class="inline-link">Sign up</a> for the email list to keep up with future events.</p>
+
+<h4  class="week-number">Week 8: Balance in play and learning</h4>
+<h2 class="reading-name">Asymmetrical Games</h2>
+<p>This meeting is dedicated to discussing (and playing!) asymmetrical games like Simon Says, Dungeons &amp; Dragons, and many others. We'll talk about our favorite asymmetrical games, what we liked about playing them, and the design of such games and how it relates to the design of learning environments.</p>
+<p class="all-caps-tracked-out mar-top-sm">Side-quests:</p>
+  <ul>
+    <li>Mario Party 4: <a class="inline-link" href="https://www.youtube.com/watch?v=l8Jnw7Etkg4">All 1 v 3 mini games</a></li>
+    <li>Link Header <a class="inline-link" href="https://www.example.com">Link description</a></li>
+<a href="https://www.eventbrite.com/e/pedagogy-play-lab-montessori-reggio-anji-tickets-35832502981"><button>RSVP</button></a><h4 class="date">Wednesday, 7/12/2017, 7pm | <a href="http://eepurl.com/ciqYyT" class="inline-link">sign up</a> for emails.</h4>
+
+<hr />
+<h2>Archive</h2>
+<div class="archive">
+
+<h4  class="week-number">Week 1: Incentives in games</h4>
+<h2 class="reading-name"><a class="archive" href="http://www.digra.org/wp-content/uploads/digital-library/11310.20247.pdf">Game Reward Systems: Gaming Experiences and Social Meanings</a><span> | Hao Wang &amp; Chuen-Tsai Sun</span></h2>
+<p>The authors give an overview of how various video game reward systems provide positive experiences to players, and propose classifications for rewards and reward characteristics for further analysis.</p>
+<a class="archive" href="http://www.digra.org/wp-content/uploads/digital-library/11310.20247.pdf"><button class="archive" >Full Text</button></a><h4 class="date happened">Thursday, 12/8/2016, 7pm</h4>
+
+<h4  class="week-number">Week 2: Socilogical backbone to play</h4>
+<h2 class="reading-name"><a class="archive" href="http://art.yale.edu/file_columns/0000/1474/homo_ludens_johan_huizinga_routledge_1949_.pdf">Homo Ludens</a><span> | J Huizinga</span></h2>
+<p>Homo Ludens is a book written in 1938 by Dutch historian and cultural theorist Johan Huizinga. It discusses the importance of the play element of culture and society. Huizinga suggests that play is primary to and a necessary (though not sufficient) condition of the generation of culture.</p>
+<a class="archive" href="http://art.yale.edu/file_columns/0000/1474/homo_ludens_johan_huizinga_routledge_1949_.pdf"><button class="archive" >Full Text</button></a><h4 class="date happened">Thursday, 1/5/2017, 7pm</h4>
+
+<h4  class="week-number">Week 3: The value and pedagogy of soft skills.</h4>
+<h2 class="reading-name"><a class="archive" href="http://paultough.com/helping/">Helping Children Succeed</a><span> | Paul Tough</span></h2>
+<p>What should we do to improve the lives of children growing up in adversity? From the best-selling author of How Children Succeed, a handbook to guide readers through the new science of success.</p>
+<a class="archive" href="http://paultough.com/helping/"><button class="archive" >Full Text</button></a><h4 class="date happened">Thursday, 1/19/2017, 7pm</h4>
+
+<h4  class="week-number">Week 4: Prototyping learning</h4>
+<h2 class="reading-name">Design a space for learning</h2>
+<p>Based on the readings and discussions to date, design a space for learning based on play. The deliverable might take the form of a scale model, business plan, website, lesson plan, painting, manifesto, or whatever you see fit.</p>
+<h4 class="date happened">Thursday, 2/2/2017, 7pm</h4>
+
+  <h4  class="week-number">Week 5: From Constructivism to Constructionism</h4>
+<h2 class="reading-name">Mindstorms<span> | Seymour Papert</span></h2>
+<p>Mindstorms has two central themes: that children can learn to use computers in a masterful way and that learning to use computers can change the way they learn everything else.</p>
+<p class="all-caps-tracked-out mar-top-sm">Side-quests:</p>
+<ul>
+<li>Exploring Papert's roots: <a class="inline-link" href="https://www.researchgate.net/publication/250182168_The_Essential_Piaget">The essential piaget</a></li>
+  <li>10 years after "Mindstorms": <a class="inline-link" href="http://isites.harvard.edu/fs/docs/icb.topic1463867.files/T550_2014_Week_2.pdf">The Children's Machine</a></li>
+  <li>Work inspired by constructionism: <a class="inline-link" href="http://web.media.mit.edu/~edith/publications/2007.explo.ofarti.pdf">Experiences of Artifacts Edith Ackermann</a></li>
+  <li>Example of constructionism in the classroom: <a class="inline-link" href="https://vimeo.com/151124093">Multidigit Division</a></li>
+</ul>
+
+<p class="all-caps-tracked-out">Reflection activity:<p>
+<p>What is an object you loved as a child. How did you understand the rest of the world through that object?</p>
+<a href="https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxha2hsYWdoZWF8Z3g6NzgyOWYxNWNjMjE5ZjVh"><button>&#10228 Full Text</button></a><h4 class="date happened">Friday, 3/24/2017, 7pm | <a href="http://eepurl.com/ciqYyT" class="inline-link">sign up</a> for emails.</h4>
+
+<h4  class="week-number">Week 6: The theory of games</h4>
+<h2 class="reading-name">The Rules of Play<span> | Katie Salen Tekinbaş and Eric Zimmerman | Unit 2: Rules</span></h2>
+<p>As pop culture, games are as important as film or television - but game design has yet to develop a theoretical framework or critical vocabulary. Rules of Play presents a unified model for looking at all kinds of games.</p>
+
+<p class="all-caps-tracked-out mar-top-sm">Side-quests:</p>
+<ul>
+  <li>Another game design book : <a class="inline-link" href="http://www.sg4adults.eu/files/art-game-design.pdf">The Art of Game Design</a> (Chapters 8, 9, 14, and 30)</li>
+  <li>Analyzing Play : Unit 3 | Play (same link as below)</li>
+  <li><a class="inline-link" href="http://uncommonplaces.com/2016/04/sex-death-survival/">Designing Sex, Death, and Survival in the 21st Century</a> by Ida Benedetto
+</ul>
+<p class="all-caps-tracked-out">Reflection activity:</p>
+<p>Redesign a part of a game you love or a game you hate based on something you found interesting from the text.</p>
+<a href="https://gamifique.files.wordpress.com/2011/11/1-rules-of-play-game-design-fundamentals.pdf"><button>&#10228 Full Text</button></a><h4 class="date happened">Thursday, 4/6/2017, 7pm | <a href="http://eepurl.com/ciqYyT" class="inline-link">sign up</a> for emails.</h4>
+
+<h4  class="week-number">Week 7: Montessori, Reggio, Anji</h4>
+<h2 class="reading-name">Play-Driven Early Childhood Education Models</h2>
+<p>This meeting is dedicated to discussing three notable play-driven models for early childhood education. We will watch some videos and then open up to a discussion.</p>
+<p class="all-caps-tracked-out mar-top-sm">Side-quests:</p>
+  <ul>
+    <li>Montessori: <a class="inline-link" href="https://montessorium.com/">Montessorium (A collection of resources)</a></li>
+    <li>Reggio Emilia: <a class="inline-link" href="https://www.theatlantic.com/national/archive/2013/01/reggio-emilia-from-postwar-italy-to-nycs-toniest-preschools/267204/">From Postwar Italy to NYC's Toniest Preschools</a></li>
+    <li><a href="http://www.anjiplay.com/" class="inline-link">Anji Play</a></li>
+<a href="https://www.eventbrite.com/e/pedagogy-play-lab-montessori-reggio-anji-tickets-35832502981"><button>RSVP</button></a><h4 class="date happened">Wednesday, 7/12/2017, 7pm | <a href="http://eepurl.com/ciqYyT" class="inline-link">sign up</a> for emails.</h4>
+
+</div>
+</div>
+
+<?php snippet('footer') ?>
